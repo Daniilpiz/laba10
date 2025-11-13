56a568f93a5da0749d43ab764f499a766efeff50
Даниил <daniildaniil2107@gmail.com>
2025-11-13 11:31:11 +0300

    лоука

--- lab10.py
+++ lab10.py
@@ -1,7 +1,7 @@
 from func import *
 from generatives import *
 
-def verts(G):
+def verts(G_0):
     start_0 = int(input("Введите вершину, с которой начать"))
 
     distances_0 = bfsd(G_0, start_0)
@@ -14,13 +14,11 @@ def verts(G):
 
 if __name__ =="__main__":
     
-    G_0 = generator_smezh(4)
-    verts(G_0)
-
-    start_1 = int(input("Введите вершину, с которой начать"))
-    G = generate_adjacency_matrix_neor(4, density=0.3,)
+    G = generator_smezh(4)
     verts(G)
 
-    start_2 = int(input("Введите вершину, с которой начать"))
-    G_1 = generate_adjacency_matrix_or(4, density=0.6,)
+    G_1 = generate_adjacency_matrix_neor(4, density=0.3,)
     verts(G_1)
+
+    G_2 = generate_adjacency_matrix_or(4, density=0.6,)
+    verts(G_2)
